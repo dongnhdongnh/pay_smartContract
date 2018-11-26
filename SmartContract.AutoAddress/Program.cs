afefd6d6c8998cc7236883b359d1684ef7742db6
huan92 <huannn@vakaxa.com>
2018-11-26 13:44:02 +0700

    service smartcontract

---
+++ SmartContract.AutoAddress/Program.cs
@@ -0,0 +1,12 @@
+﻿using System;
+
+namespace SmartContract.AutoAddress
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Hello World!");
+        }
+    }
+}
